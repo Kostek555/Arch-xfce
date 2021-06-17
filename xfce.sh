90bff7e7375e0c446fed102aaaba1f2cff011ecd
Kostek555 <kostek55@protonmail.ch>
2021-06-17 12:27:48 +0200

    zmiana 2

--- xfce.sh
+++ xfce.sh
@@ -20,13 +20,12 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 
 
-pacman -S xorg grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers xdg-user-dirs xdg-utils gvfs gvfs-afc gvfs-smb gvfs-gphoto2 gvfs-mtp gvfs-goa gvfs-nfs gvfs-google alsa-utils bash-completion rsync acpi acpi_call dnsmasq sof-firmware acpid ntfs-3g terminus-font xterm xorg-twm xorg-xclock xfce4 xfce4-goodies pavucontrol pulseaudio-alsa pavucontrol pulseaudio-alsa chromium file-roller unrar p7zip unace lrzip lightdm lightdm-gtk-greeter leafpad arch-install-scripts bash-completion ttf-inconsolata gparted util-linux arc-gtk-theme arc-icon-theme gtk-engine-murrine vim jre-openjdk jdk-openjdk sudo --noconfirm
+pacman -S xorg networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools  base-devel linux-headers xdg-user-dirs xdg-utils gvfs gvfs-afc gvfs-smb gvfs-gphoto2 gvfs-mtp gvfs-goa gvfs-nfs gvfs-google alsa-utils bash-completion  acpi acpi_call dnsmasq sof-firmware acpid ntfs-3g terminus-font xterm xorg-twm xorg-xclock xfce4 xfce4-goodies pavucontrol pulseaudio-alsa  chromium file-roller unrar p7zip unace lrzip lightdm lightdm-gtk-greeter leafpad arch-install-scripts ttf-inconsolata gparted util-linux arc-gtk-theme arc-icon-theme gtk-engine-murrine vim jre-openjdk jdk-openjdk sudo lightdm lightdm-gtk-greeter	lightdm-gtk-greeter-settings --noconfirm
 
 
 
-grub-install /dev/sdx
-grub-mkconfig -o /boot/grub/grub.cfg
 
+systemctl enable lightdm
 systemctl enable NetworkManager
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
