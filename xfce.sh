ffbe87f7bd187f86a6fd419586a2e7240a01e40b
Kostek555 <kostek55@protonmail.ch>
2021-06-17 11:49:01 +0200

    zmiana 1

--- xfce.sh
+++ xfce.sh
@@ -18,12 +18,11 @@ echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
-echo root:password | passwd
 
-pacman -S grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers xdg-user-dirs xdg-utils gvfs gvfs-afc gvfs-smb gvfs-gphoto2 gvfs-mtp gvfs-goa gvfs-nfs gvfs-google alsa-utils bash-completion rsync acpi acpi_call dnsmasq sof-firmware acpid ntfs-3g terminus-font xterm xorg-twm xorg-xclock xfce4 xfce4-goodies pavucontrol pulseaudio-alsa pavucontrol pulseaudio-alsa chromium file-roller unrar p7zip unace lrzip lightdm lightdm-gtk-greeter leafpad arch-install-scripts bash-completion ttf-inconsolata gparted util-linux arc-gtk-theme arc-icon-theme gtk-engine-murrine vim jre-openjdk jdk-openjdk sudo --noconfirm
 
-pacman -S --noconfirm xf86-video-intel
-pacman -S --noconfirm xf86-video-nouveau
+pacman -S xorg grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers xdg-user-dirs xdg-utils gvfs gvfs-afc gvfs-smb gvfs-gphoto2 gvfs-mtp gvfs-goa gvfs-nfs gvfs-google alsa-utils bash-completion rsync acpi acpi_call dnsmasq sof-firmware acpid ntfs-3g terminus-font xterm xorg-twm xorg-xclock xfce4 xfce4-goodies pavucontrol pulseaudio-alsa pavucontrol pulseaudio-alsa chromium file-roller unrar p7zip unace lrzip lightdm lightdm-gtk-greeter leafpad arch-install-scripts bash-completion ttf-inconsolata gparted util-linux arc-gtk-theme arc-icon-theme gtk-engine-murrine vim jre-openjdk jdk-openjdk sudo --noconfirm
+
+
 
 grub-install /dev/sdx
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -33,10 +32,7 @@ systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable acpid
 
-useradd -m -g users -G wheel -s /bin/bash greg
-echo greg:password | passwd1
 
-echo "greg ALL=(ALL) ALL" >> /etc/sudoers.d/greg
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
 
