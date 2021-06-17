84e4d20a7a6240907559cbbee71884d5c4f1221a
Kostek555 <kostek55@protonmail.ch>
2021-06-17 13:28:50 +0200

    zmiana 3

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Arch-inatall
-W tym repozytorium znajdziesz skrypty pakietów do podstawowej instalacji Arch Linux xfce, uczyń skrypt wykonywalnym za pomocą chmod +x nazwa_skryptu, a następnie uruchom za pomocą ./nazwa_skryptu.Skrypt do podstawowej instalacji na MBR/Legacy. Pamiętaj, że pierwsza część instalacji Arch Linux jest ręczna, to znaczy, że będziesz musiał samodzielnie podzielić, sformatować i zamontować dysk. Zainstaluj pakiety podstawowe i upewnij się, że zawiera git, aby móc sklonować repozytorium w chroot.
+W tym repozytorium znajdziesz skrypt pakietów do podstawowej instalacji Arch Linux xfce, uczyń skrypt wykonywalnym za pomocą chmod +x nazwa_skryptu, a następnie uruchom za pomocą ./nazwa_skryptu.Skrypt do podstawowej instalacji na MBR. Pamiętaj, że pierwsza część instalacji Arch Linux jest ręczna, to znaczy, że będziesz musiał samodzielnie podzielić, sformatować i zamontować dysk. 
+1 Zainstaluj pakiety podstawowe pacstrap /mnt base linux linux-firmware git vim (lub nano) intel-ucode (lub amd-ucode). 
+2  Wygeneruj plik FSTAB za pomocą genfstab -U /mnt >> /mnt/etc/fstab
+3 Chrootuj za pomocą arch-chroot /mnt
 
-Sformatuj partycje
-Zamontuj pattycje
-Zainstaluj pakiety podstawowe w /mnt (pacstrap /mnt base linux linux-firmware git vim (lub nano) intel-ucode (lub amd-ucode))
-Wygeneruj plik FSTAB za pomocą genfstab -U /mnt >> /mnt/etc/fstab
-Chrootuj za pomocą arch-chroot /mnt
-Pobierz repozytorium git .
-
+4 Zainstaluj grub komendą pacman -S grub
+za x wpisz dysk na którym instalujesz grub
+grub-install /dev/sdx   
+grub-mkconfig -o /boot/grub/grub.cfg
+ 
--- xfce.sh
+++ xfce.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-
-#Reflector
-
-pacman -Sy
-pacman -S reflector --noconfirm
-
-reflector --score 5 --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
-
 ln -s /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
 hwclock --systohc --utc
 sed -i '177s/.//' /etc/locale.gen &&  sed -i '390s/.//' /etc/locale.gen
@@ -20,16 +12,15 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 
 
-pacman -S xorg networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools  base-devel linux-headers xdg-user-dirs xdg-utils gvfs gvfs-afc gvfs-smb gvfs-gphoto2 gvfs-mtp gvfs-goa gvfs-nfs gvfs-google alsa-utils bash-completion  acpi acpi_call dnsmasq sof-firmware acpid ntfs-3g terminus-font xterm xorg-twm xorg-xclock xfce4 xfce4-goodies pavucontrol pulseaudio-alsa  chromium file-roller unrar p7zip unace lrzip lightdm lightdm-gtk-greeter leafpad arch-install-scripts ttf-inconsolata gparted util-linux arc-gtk-theme arc-icon-theme gtk-engine-murrine vim jre-openjdk jdk-openjdk sudo lightdm lightdm-gtk-greeter	lightdm-gtk-greeter-settings --noconfirm
+pacman -S networkmanager network-manager-applet dialog wpa_supplicant mtools base-devel linux-headers
 
+systemctl enable NetworkManager
 
-
+pacman -S xorg gvfs gvfs-afc gvfs-smb gvfs-gphoto2 gvfs-mtp gvfs-goa gvfs-nfs gvfs-google ntfs-3g gvfs gvfs-afc gvfs-smb gvfs-gphoto2 gvfs-mtp gvfs-goa gvfs-nfs gvfs-google xterm xorg-twm xorg-xclock xfce4 xfce4-goodies pavucontrol pulseaudio-alsa pavucontrol pulseaudio-alsa chromium file-roller unrar p7zip unace lrzip lightdm lightdm-gtk-greeter leafpad arch-install-scripts bash-completion mtools dosfstools reflector xdg-user-dirs xdg-utils acpi acpi_call  terminus-font ttf-inconsolata gparted util-linux arc-gtk-theme arc-icon-theme gtk-engine-murrine leafpad  jre-openjdk jdk-openjdk --noconfirm
 
 systemctl enable lightdm
-systemctl enable NetworkManager
-systemctl enable reflector.timer
 systemctl enable fstrim.timer
-systemctl enable acpid
+
 
 
 
