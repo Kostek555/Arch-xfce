135cc3d99d4520c3908598c1fbbb9a20c31677e2
Kostek555 <kostek55@protonmail.ch>
2021-06-20 10:44:05 +0200

    R xf-ce

--- xfce.sh
+++ xfce.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+#---------Reflector
+
 pacman -Sy
 pacman -S reflector --noconfirm
+reflector --score 5 --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
 
-reflector --score 5 --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
+#---------Loaltime-------------------------
 
 ln -s /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
 hwclock --systohc --utc
+
 sed -i '177s/.//' /etc/locale.gen &&  sed -i '390s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=pl_PL.UTF-8" >> /etc/locale.conf
@@ -17,7 +21,7 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 
 
-# GRUB
+# --------Grub---------------------
 
 pacman -S grub efibootmgr --noconfirm
 grub-install /dev/sdc
@@ -25,19 +29,39 @@ grub-install --target=x86_64-efi --efi-directory /boot --boot-directory /boot --
 grub-mkconfig -o /boot/grub/grub.cfg
 
 
+#--------Network------------------
 
-
-pacman -S networkmanager network-manager-applet dialog wpa_supplicant mtools base-devel linux-headers --noconfirm
+pacman -S networkmanager network-manager-applet dialog wpa_supplicant mtools base-devel --noconfirm
 
 systemctl enable NetworkManager
 
-pacman -S xorg xterm xorg-twm xorg-xclock gvfs gvfs-afc gvfs-smb gvfs-gphoto2 gvfs-mtp gvfs-goa gvfs-nfs gvfs-google ntfs-3g gvfs gvfs-afc gvfs-smb gvfs-gphoto2 gvfs-mtp gvfs-goa gvfs-nfs gvfs-google lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings vim sudo --noconfirm
+#--------Xorg----------------------
+
+pacman -S xorg xterm xorg-twm xorg-xclock 
+
+#----------System---------------
+
+pacman -S gvfs gvfs-afc gvfs-smb gvfs-gphoto2 gvfs-mtp gvfs-goa gvfs-nfs gvfs-google ntfs-3g
+
+#---------Lightdm--------------
+
+pacman -S  lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings --noconfirm
 
 systemctl enable lightdm
 
-pacman -S xfce4 xfce4-goodies pavucontrol pulseaudio-alsa chromium file-roller unrar p7zip unace lrzip leafpad arch-install-scripts bash-completion mtools dosfstools xdg-user-dirs xdg-utils acpi acpi_call  terminus-font ttf-inconsolata gparted util-linux arc-gtk-theme arc-icon-theme gtk-engine-murrine leafpad  jre-openjdk jdk-openjdk --noconfirm
+#-----------Xfce------------------
+
+pacman -S xfce4 xfce4-goodies pavucontrol pulseaudio-alsa chromium file-roller unrar p7zip unace lrzip leafpad arch-install-scripts bash-completion mtools dosfstools xdg-user-dirs xdg-utils acpi acpi_call terminus-font ttf-inconsolata gparted util-linux arc-gtk-theme arc-icon-theme gtk-engine-murrine leafpad jre-openjdk jdk-openjdk vim sudo --noconfirm
 
 systemctl enable fstrim.timer
+systemctl enable reflector.timer
 
-printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+#---------linux-headers---------
+
+pacman -S linux-headers --noconfirm
+
+#=========================
+cd .. 
+
+printf "\e[1;32mDone! Type exit, umount -R /mnt and reboot.\e[0m"
 
